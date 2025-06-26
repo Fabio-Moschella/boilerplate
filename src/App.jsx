0a572303f8cc48a274bbbe39883c03af27d98934
arielmusa <arielmusainfo@gmail.com>
2025-06-26 11:59:55 +0200

    feat: add new text in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  return <h1>ciao</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A animi ipsum
+        quae ullam nisi dolorem illum corporis, nobis sed, beatae magnam debitis
+        omnis illo eligendi ipsam quidem ducimus at? Velit!
+      </p>
+    </>
+  );
 }
 
 export default App;
