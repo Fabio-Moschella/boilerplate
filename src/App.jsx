b951fdecb90213e797ce7515c83be170ca476e7a
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-06-26 12:03:13 +0200

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1>ciao oggi</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. A animi ipsum
         quae ullam nisi dolorem illum corporis, nobis sed, beatae magnam debitis
